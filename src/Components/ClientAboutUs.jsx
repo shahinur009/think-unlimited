30ae1af18bfeafcdf165576d84a00c414dfe65fc
shahin009 <shaheenmis170879@gmail.com>
2024-08-01 05:29:04 +0600

    task done

--- src/Components/ClientAboutUs.jsx
+++ src/Components/ClientAboutUs.jsx
@@ -1,6 +1,45 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
 import ClientCard from "./ClientCard";
+import axios from "axios";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/thumbs';
+import { FreeMode, Navigation, Thumbs, Autoplay, Pagination } from 'swiper/modules';
 
 const ClientAboutUs = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('client.json');
+                setData(response.data);
+                setLoading(false);
+            } catch (error) {
+                setError(error);
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
     return (
         <>
             <section className="bg-[#0E0E0E] text-white py-10">
@@ -8,14 +47,31 @@ const ClientAboutUs = () => {
                     <div className='text-center'>
                         <p className='font-extrabold text-8xl text-[#E26E23] uppercase -ml-10'><span className="text-white">what</span> clients</p>
                         <p className='font-extrabold text-8xl -mt-4 uppercase'>say <span className="text-[#E26E23] ">about</span> us </p>
-
                     </div>
                     <div>
                         <p className="text-[#E26E23] text-[150px] font-extrabold mt-5">?</p>
                     </div>
                 </div>
-                <div>
-                    <ClientCard />
+                <div className="max-w-full flex justify-center items-center">
+                    <Swiper
+                        slidesPerView={1}
+                        spaceBetween={30}
+                        loop={true}
+                        pagination={{
+                          clickable: true,
+                        }}
+                        navigation={true}
+                        modules={[Pagination, Navigation]}
+                        className="mySwiper"
+                    >
+                        <div className="flex justify-center items-center">
+                            {data.map(item => (
+                                <SwiperSlide key={item.id}>
+                                    <ClientCard item={item} />
+                                </SwiperSlide>
+                            ))}
+                        </div>
+                    </Swiper>
                 </div>
             </section>
         </>
--- src/Components/ClientCard.jsx
+++ src/Components/ClientCard.jsx
@@ -1,10 +1,48 @@
+import { Rating } from '@smastrom/react-rating';
+import PropTypes from 'prop-types';
 
-const ClientCard = () => {
+const ClientCard = ({ item }) => {
+    const { name, image, designation, description, rating } = item;
+    console.log(item)
     return (
-        <div>
-            
+        // < !--component -- >
+        <div className="min-h-screen flex flex-col justify-center sm:py-12 bg-[#0E0E0E] text-white py-10">
+
+            <div className="py-3 sm:max-w-xl sm:mx-auto">
+                <div className="shadow-lg border-gray-100 max-h-80	 border sm:rounded-3xl p-8 flex space-x-8 bg-[#E26E23]">
+                    <div className="h-48 overflow-visible w-1/2 bg-yellow-400 rounded-badge">
+                        <img className="rounded-3xl shadow-lg" src={image} alt="" />
+                    </div>
+                    <div className="flex flex-col w-1/2 space-y-4">
+                        <div className="flex justify-between items-start">
+                            <h2 className="text-3xl font-bold">{name}</h2>
+                            <div className="bg-yellow-400 font-bold rounded-xl p-2">{rating}</div>
+                        </div>
+                        <div>
+                            <div className="text-xl text-white">{designation}</div>
+                        </div>
+                        <p className=" text-white max-h-40 overflow-y-hidden">{description}</p>
+                        <div className="flex text-2xl font-bold text-a">
+                            <Rating
+                                style={{ maxWidth: 180 }}
+                                value={rating}
+                                readOnly></Rating>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     );
 };
-
+ClientCard.propTypes = {
+    item: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        designation: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired
+    }).isRequired
+};
 export default ClientCard;
